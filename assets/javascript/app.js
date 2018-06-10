04329289fe133f0b38f696dc29075d6fa44035c8
jterry63 <jordankterry@outlook.com>
2018-06-09 21:11:26 -0600

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,6 +28,7 @@ function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
+        window.location.reload();
     });
 }
 
