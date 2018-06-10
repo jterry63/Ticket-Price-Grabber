f637323bc28b9aa83f3a806883ff279ad0ec4f89
jterry63 <jordankterry@outlook.com>
2018-06-09 18:31:39 -0600

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,8 +8,19 @@ var config = {
     messagingSenderId: "34076773600"
 };
 firebase.initializeApp(config);
-
 var database = firebase.database();
+
+
+//Google Auth
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    $(".g-signin2").css("display", "none");
+    $(".application").css("display", "block");
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
 
 var lookup = "";
 var auth = "";
