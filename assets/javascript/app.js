2f4ae2ee2116cd138cfb8e5bbd4b9c02c3be884d
jterry63 <jordankterry@outlook.com>
2018-06-09 20:50:15 -0600

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,6 +24,13 @@ function onSignIn(googleUser) {
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
 var lookup = "";
 var auth = "";
 
